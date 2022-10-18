1511b2e20b3b3ceb99a1a94ff915a7000b0b913e
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-10-17 18:41:56 -0700

    Add test: get merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,20 @@ describe "Merchant API" do
       expect(merchant[:name]).to be_a(String)
     end 
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(Integer)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end 
 end
